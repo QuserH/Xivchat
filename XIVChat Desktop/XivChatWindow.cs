2a61662842a1e2d8d4e597562a9021c6859df152
Anna <git@anna.lgbt>
2021-02-07 00:18:14 -0500

    fix: give icon to all windows

--- XIVChat Desktop/XivChatWindow.cs
+++ XIVChat Desktop/XivChatWindow.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace XIVChat_Desktop {
     public class XivChatWindow : Window {
         // ReSharper disable once MemberCanBeProtected.Global
         public XivChatWindow() {
+            this.Icon = new BitmapImage(new Uri("pack://application:,,,/Resources/logo.ico"));
             this.SetValue(TextOptions.TextRenderingModeProperty, TextRenderingMode.Auto);
             this.SetValue(TextOptions.TextFormattingModeProperty, TextFormattingMode.Display);
             this.SetValue(TextOptions.TextHintingModeProperty, TextHintingMode.Fixed);
