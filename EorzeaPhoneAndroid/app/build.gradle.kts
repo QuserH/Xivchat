ca91361d12a22fe95b9eb2d9d1d6c4e17715ae7b
QuserH <1149697091@qq.com>
2026-08-30 17:15:42 +0800

    版本号 0.7.268/290（0.7.268）

--- EorzeaPhoneAndroid/app/build.gradle.kts
+++ EorzeaPhoneAndroid/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.quserh.eorzeaphone"
         minSdk = 24
         targetSdk = 36
-        versionCode = 289
-        versionName = "0.7.267"
+        versionCode = 290
+        versionName = "0.7.268"
     }
 
     buildFeatures {
